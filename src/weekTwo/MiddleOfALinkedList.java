e09826c602a4cf3ffa0a06e58d78ea5c34ed2fb2
hannasamuel20 <hanna17st@gmail.com>
2021-01-15 18:22:15 +0300

    revereLinkedList

---
+++ src/weekTwo/MiddleOfALinkedList.java
@@ -0,0 +1,27 @@
+package weekTwo;
+
+public class MiddleOfALinkedList {
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+      public ListNode middleNode(ListNode head) {
+          ListNode fast=head;
+          ListNode slow= head;
+          while(fast.next!=null){
+              if(fast.next.next==null){
+                  slow=slow.next;
+                  break;
+
+              }
+              fast=fast.next.next;
+              slow=slow.next;
+          }
+          return slow;
+
+      }
+  }
+}
