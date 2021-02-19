32b641660142467135b5b4382f00f6376f84286a
hannasamuel20 <hanna17st@gmail.com>
2021-02-19 09:28:48 +0300

    crawler

---
+++ src/weekThree/CrawlerLogFolder.java
@@ -0,0 +1,26 @@
+package weekThree;
+
+public class CrawlerLogFolder {
+    public int minOperations(String[] logs) {
+        int count = 0;
+        for(int i=0; i<logs.length;i++){
+            if(logs[i].equals("../")){
+                if(count<=0){
+                    count=0;
+                }
+                else{
+                    count--;
+                }
+
+            }
+            else if(logs[i].equals("./")){
+
+            }
+            else{
+                count++;
+            }
+        }
+        return count;
+
+    }
+}
